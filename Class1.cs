cc7b4306344ba4e52bf33535d7925fe98beb73aa
helloworld466 <akshayama466@gmail.com>
2020-09-04 03:54:59 +0000

    d

---
+++ Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ClassLibraryProject
+{
+    public class Class1
+    {
+	static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Aspire!");
+	    Console.WriteLine("The current time is " + DateTime.Now);
+        }
+    }
+}
